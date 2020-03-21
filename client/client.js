1412860db11e261c5d7f61b86e5adc08f7288847
Lukas Schöbel <56588215+lukaschoebel@users.noreply.github.com>
2020-03-21 11:14:07 +0100

    add unary rpc call

--- client/client.js
+++ client/client.js
@@ -1,11 +1,34 @@
 var grpc = require('grpc');
-var services = require('../server/proto/weather_grpc_pb');
+var weather = require('../server/proto/weather_pb');
+var service = require('../server/proto/weather_grpc_pb');
 
 function main() {
-    var client = services.WeatherServiceClient(
+    var client = new service.WeatherServiceClient(
         'localhost:50051',
         grpc.credentials.createInsecure()
     );
+    
+    // Setup Location
+    var location = new weather.Location();
+    location.setCity('London');
+    location.setCountry('UK');
+    
+    // Setup Request
+    var request = new weather.WeatherRequest();
+    request.setLocation(location);
+
+    client.checkWeatherToday(request, (error, response) => {
+        if (!error) {
+            console.log('################## RESULT ##################');
+            console.log(response.getResult());
+            console.log(response.getTodaystemperature());
+            console.log(response.getTodaysuvindex());
+            console.log('############################################');
+        } else {
+            console.log(error);
+        }
+    });
+
 }
 
-main()
+main();
--- server/index.js
+++ server/index.js
@@ -1,12 +1,34 @@
 var grpc = require('grpc');
+var weather = require('../server/proto/weather_pb');
+var service = require('../server/proto/weather_grpc_pb');
+
+// Implementation of RPC Method
+function checkWeatherToday(call, callback) {
+    var weatherResponse = new weather.WeatherResponse();
+    
+    weatherResponse.setResult(
+        `It's always sunny in ${call.request.getLocation().getCity()}, ${call.request.getLocation().getCountry()}`
+    );
+
+    weatherResponse.setTodaystemperature(
+        "Temperature: 25°C."
+    );
+
+    weatherResponse.setTodaysuvindex(
+        "UV-Index: 1."
+    );
+
+    callback(null, weatherResponse);
+
+}
 
 function main() {
     var server = new grpc.Server();
+    server.addService(service.WeatherServiceService, {checkWeatherToday: checkWeatherToday})
 
     server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure());
     server.start();
     console.log('Server running on port 127.0.0.1:50051');
-    
 
 }
 
